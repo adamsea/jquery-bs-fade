0da0dbdcc57eafb9b2d7b711c9290e0f925a491a
Eric Adams <adams.ea@gmail.com>
2015-05-04 12:46:12 -0400

    Initial commit of plugin code

---
+++ jquery.bs-fade.js
@@ -0,0 +1,63 @@
+/**
+ * Replacements for .fadeIn and .fadeOut that apply
+ * CSS classes to fade and element and will show/hide
+ * the element using CSS as well.  This is not a replacement
+ * for the full functionality of .fadeIn and .fadeOut, as the
+ * timing of the animation is not set in options but rather in CSS.
+ */
+(function($) {
+
+	// From Bootstrap transition helper
+	var transitionEvent = (function() {
+		var el = document.createElement('bootstrap');
+
+		var transEndEventNames = {
+			WebkitTransition : 'webkitTransitionEnd',
+			MozTransition    : 'transitionend',
+			OTransition      : 'oTransitionEnd otransitionend',
+			transition       : 'transitionend'
+		};
+
+		for (var name in transEndEventNames) {
+			if (el.style[name] !== undefined) {
+				return transEndEventNames[name];
+			}
+		}
+
+		return false;
+	}());
+
+	/**
+	 * jQuery.fn.cssFadeOut
+	 */
+	$.fn.cssFadeOut = function(callback) {
+		var $elem = $(this);
+		$elem
+			.removeClass('in')
+			.one(transitionEvent, function() {
+				$elem.addClass('hide');
+				if (typeof callback === 'function') {
+					return callback.call(this);
+				}
+			});
+	};
+
+	/**
+	 * jQuery.fn.cssFadeIn
+	 */
+	$.fn.cssFadeIn = function(callback) {
+		var $elem = $(this);
+		$elem
+			.removeClass('hide');
+		requestAnimationFrame(function() {
+			$elem
+				.addClass('in')
+				.one(transitionEvent, function() {
+					if (typeof callback === 'function') {
+						return callback.call(this);
+					}
+				});
+		});
+	};
+
+}(window.jQuery));
