58c7cbe4821d6bd2e9929a7605bf2333284c67c7
Eric Adams <adams.ea@gmail.com>
2015-05-05 13:13:59 -0400

    Return the element for chaining, of course

--- jquery.bs-fade.js
+++ jquery.bs-fade.js
@@ -7,57 +7,59 @@
  */
 (function($) {
 
-	// From Bootstrap transition helper
-	var transitionEvent = (function() {
-		var name,
-			el = document.createElement('bootstrap'),
-			transEndEventNames = {
-				WebkitTransition : 'webkitTransitionEnd',
-				MozTransition    : 'transitionend',
-				OTransition      : 'oTransitionEnd otransitionend',
-				transition       : 'transitionend'
-			};
+    // From Bootstrap transition helper
+    var transitionEvent = (function() {
+        var name,
+            el = document.createElement('bootstrap'),
+            transEndEventNames = {
+                WebkitTransition : 'webkitTransitionEnd',
+                MozTransition    : 'transitionend',
+                OTransition      : 'oTransitionEnd otransitionend',
+                transition       : 'transitionend'
+            };
 
-		for (name in transEndEventNames) {
-			if (el.style[name] !== undefined) {
-				return transEndEventNames[name];
-			}
-		}
+        for (name in transEndEventNames) {
+            if (el.style[name] !== undefined) {
+                return transEndEventNames[name];
+            }
+        }
 
-		return false;
-	}());
+        return false;
+    }());
 
-	/**
-	 * jQuery.fn.cssFadeOut
-	 */
-	$.fn.cssFadeOut = function(callback) {
-		var $elem = $(this);
-		$elem
-			.removeClass('in')
-			.one(transitionEvent, function() {
-				$elem.addClass('hide');
-				if (typeof callback === 'function') {
-					return callback.call(this);
-				}
-			});
-	};
+    /**
+     * jQuery.fn.cssFadeOut
+     */
+    $.fn.cssFadeOut = function(callback) {
+        var $elem = $(this);
+        $elem
+            .removeClass('in')
+            .one(transitionEvent, function() {
+                $elem.addClass('hide');
+                if (typeof callback === 'function') {
+                    return callback.call(this);
+                }
+            });
+        return $elem;
+    };
 
-	/**
-	 * jQuery.fn.cssFadeIn
-	 */
-	$.fn.cssFadeIn = function(callback) {
-		var $elem = $(this);
-		$elem
-			.removeClass('hide');
-		window.requestAnimationFrame(function() {
-			$elem
-				.addClass('in')
-				.one(transitionEvent, function() {
-					if (typeof callback === 'function') {
-						return callback.call(this);
-					}
-				});
-		});
-	};
+    /**
+     * jQuery.fn.cssFadeIn
+     */
+    $.fn.cssFadeIn = function(callback) {
+        var $elem = $(this);
+        $elem
+            .removeClass('hide');
+        window.requestAnimationFrame(function() {
+            $elem
+                .addClass('in')
+                .one(transitionEvent, function() {
+                    if (typeof callback === 'function') {
+                        return callback.call(this);
+                    }
+                });
+        });
+        return $elem;
+    };
 
 }(window.jQuery));
